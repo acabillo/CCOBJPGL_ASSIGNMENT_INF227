8515af7470f68545b7bfce869865bdb5dcc96c87
Anthonette Mae C. Navarro <94913007+acabillo@users.noreply.github.com>
2023-05-23 12:01:42 +0800

    Create Restaurant.java

---
+++ Exercise5/model/Restaurant.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Restaurant {
+    private String color;
+    private String BestSeller;
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBestSeller() {
+        return this.BestSeller;
+    }
+
+    public void setBestSeller(String bestSeller) {
+        this.BestSeller = bestSeller;
+    }
+    
+}
